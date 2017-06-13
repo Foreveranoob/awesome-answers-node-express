a4c12ed33ffda6a652963f10f91128a668bd508d
Chris Ryan <chrisryan@hotmail.ca>
2017-06-13 16:10:42 -0700

    added show page for questions and their routes

--- routes/questions.js
+++ routes/questions.js
@@ -15,4 +15,39 @@ router.get('/', (req, res) => {
     });
 });
 
+// questions#show PATH: /questions/:id METHOD: get
+// when declaring a function, prefix with the `async`
+// keyword to make it an async/await function.
+// async/await functions can treat Promises as if
+// they are synchronous.
+// They always return their value wrapped in a Promise. In
+// other words, the return value will be the resolved value
+// of the promise.
+
+// async function () {}
+router.get('/:id', async (req, res) => {
+  // To get params from your url (i.e. /:id),
+  // grab them the params property on the request object
+  // as shown below ð
+  const { id } = req.params;
+  const question = await Question.findById(id);
+  res.render('questions/show', {question: question});
+});
+
+// ð version of the above ð route without an
+// async/await function
+/*
+router.get('/:id', (req, res) => {
+  // To get params from your url (i.e. /:id),
+  // grab them the params property on the request object
+  // as shown below ð
+  const { id } = req.params;
+  const question = Question
+    .findById(id)
+    .then(question => {
+      res.send(question);
+    });
+});
+*/
+
 module.exports = router;
